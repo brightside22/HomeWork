db861f772b94f08c86b910bcd326e830974e0dfa
brightside22 <brightside227@gmail.com>
2023-10-10 21:15:21 +0300

    Home Work H10

--- src/ru/progwards/java1/lessons/basics/ReverseDigits.java
+++ src/ru/progwards/java1/lessons/basics/ReverseDigits.java
@@ -2,16 +2,7 @@ package ru.progwards.java1.lessons.basics;
 
 public class ReverseDigits {
     public static int reverseDigits(int number) {
-        System.out.print((number % 10));
-        System.out.print((number % 100 / 10));
-        System.out.print((number / 100));
-        int num = number % 10 + number % 100 / 10 + number / 100;
-        return num;
+        return number = number % 10 * 100 + number % 100 / 10 * 10 + number / 100;
     }
-
-    public static void main(String [] args) {
-        reverseDigits(843);
-    }
-
 
 }
