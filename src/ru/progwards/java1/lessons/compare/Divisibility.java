fc0d69261682a1f317304cf79857f092e81cb6c3
brightside22 <brightside227@gmail.com>
2023-10-14 16:07:05 +0300

    Home Work H13

--- src/ru/progwards/java1/lessons/compare/Divisibility.java
+++ src/ru/progwards/java1/lessons/compare/Divisibility.java
@@ -4,11 +4,16 @@ public class Divisibility {
     public static void check(int num1, int num2) {
         if (num1 != 0 & num2 != 0 & num1%num2==0)
             System.out.println(num1 + " делится нацело на " + num2);
+
         else if (num1 != 0 & num2 != 0 & num1%num2 !=0)
             System.out.println("Остаток от деления " + num1 + " на " + num2 + " равен " + (num1%num2));
 
+        else if (num1 == 0 | num2 == 0)
+            System.out.println("На ноль делить нельзя");
+
         if (num1 != 0 & num2 != 0 & num1/num2!=0)
             System.out.println("Частное от деления " + num1 + " на " + num2 + " равно " +(double)num1/num2);
+
         else
             System.out.println("На ноль делить нельзя");
     }
