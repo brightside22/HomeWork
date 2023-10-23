0a4a508fcb76e245d921871626c971bfbdaf631d
brightside22 <brightside227@gmail.com>
2023-10-23 22:05:21 +0300

    Home Work H16

--- src/ru/progwards/java1/lessons/useclasses/CountTest.java
+++ src/ru/progwards/java1/lessons/useclasses/CountTest.java
@@ -10,7 +10,7 @@ public class CountTest {
             System.out.print(count1.getCount() + " ");
         }
 
-        System.out.println("тест inc окончен");
+        System.out.println("\nтест inc окончен");
     }
 
     public static void testDec(int count) {
