c843931d2270d32d8c839c9eddf2f6b2e804cda1
brightside22 <brightside227@gmail.com>
2023-10-24 22:24:41 +0300

    Home Work H16

--- src/ru/progwards/java1/lessons/useclasses/Count.java
+++ src/ru/progwards/java1/lessons/useclasses/Count.java
@@ -1,5 +1,4 @@
 package ru.progwards.java1.lessons.useclasses;
-
 public class Count {
     int count;
 
@@ -11,34 +10,27 @@ public class Count {
         this.count = count;
     }
 
-
-    public int getCount(){
+    public int getCount() {
         return count;
     }
 
     public void inc() {
-        count = (count + 1);
+        count++;
     }
 
     public boolean dec() {
-        count = (count - 1);
-        if (count <= 0)
-            return true;
-        else return false;
+        count--;
+        return count < 0;
     }
 
-
     public static void main(String[] args) {
-       Count count = new Count(10);
+        Count count = new Count(10);
 
-        while (count.dec() != true) {
-            count.dec();
-
+        while (!count.dec()) {
             if (count.getCount() == 0) {
                 System.out.println("count равен 0");
                 break;
             }
         }
     }
-
 }
