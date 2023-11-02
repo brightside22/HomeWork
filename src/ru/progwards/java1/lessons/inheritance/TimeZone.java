65574bacf93fe481d5fe627e0cd2ec8367041e58
brightside22 <brightside227@gmail.com>
2023-11-02 16:23:49 +0300

    Home Work H17.2

--- src/ru/progwards/java1/lessons/inheritance/TimeZone.java
+++ src/ru/progwards/java1/lessons/inheritance/TimeZone.java
@@ -1,24 +1,24 @@
 package ru.progwards.java1.lessons.inheritance;
 class TimeZone {
-    static int hours;
-
-    static int minutes;
+    public int hours;
+    public int minutes;
 
     public TimeZone(int hours) {
         this.hours = hours;
-        this.minutes = 0;
     }
+
     public TimeZone(int hours, int minutes) {
         this.hours = hours;
         this.minutes = minutes;
     }
-    public static int getHours() {
+
+    public int getHours() {
         return hours;
     }
 
-    public static int getMinutes() {
-        return hours;
 
+    public int getMinutes() {
+        return minutes;
     }
 
     public TimeZone getTimeZone() {
--- src/ru/progwards/java1/lessons/inheritance/ZonedTime.java
+++ src/ru/progwards/java1/lessons/inheritance/ZonedTime.java
@@ -1,7 +1,7 @@
 package ru.progwards.java1.lessons.inheritance;
 
 public class ZonedTime extends Time {
-    TimeZone zone;
+    public static TimeZone zone;
 
     public ZonedTime(int hours, int minutes, int seconds) {
         super(hours, minutes, seconds);
@@ -9,36 +9,41 @@ public class ZonedTime extends Time {
 
     public ZonedTime(int hours, int minutes, int seconds, TimeZone zone) {
         super(hours, minutes, seconds);
-        this.hours = hours;
-        this.minutes = minutes;
-        this.seconds = seconds;
-        this.zone = zone;
+        zone = new TimeZone(0);
     }
-
 
     public TimeZone getTimeZone() {
         return zone;
     }
-    public int secondsBetween(Time time) {
-        TimeZone tmz = getTimeZone();
-        int hours1 = TimeZone.hours * 3600;
-        int minutes1 = TimeZone.minutes * 60;
-        int sec =  ZonedTime.hours *  3600 + ZonedTime.minutes  *  60 + ZonedTime.seconds - hours1 - minutes1;
-        int sec1 = Time.hours * 3600 + Time.minutes * 60 + Time.seconds;
 
-        if (sec >= sec1) {
-            return sec - sec1;
-        } else {
-            return sec1 - sec;
+
+
+    @Override
+    public int secondsBetween(Time time) {
+        int tmz1 = 0;
+        if (zone != null) {
+            TimeZone timeZone = getTimeZone();
+            tmz1 = timeZone.getHours() * 3600 + timeZone.getMinutes() * 60;
         }
-    }
+
+            int sec = time.hours * 3600 + time.minutes * 60 + time.seconds;
+            int sec1 = this.hours * 3600 + this.minutes * 60 + this.seconds - tmz1;
+
+            if (sec >= sec1) {
+                return sec - sec1;
+            } else {
+                return sec1 - sec;
+            }
+        }
+
 
     public static void main(String[] args) {
-    ZonedTime zt1 = new ZonedTime(15, 39, 42, new TimeZone(-3, 2));
-        Time t1 = new Time(22, 1, 36);
-        System.out.println(zt1.secondsBetween(t1));
-        System.out.println(ZonedTime.hours);
-        System.out.println(Time.hours);
+        ZonedTime zt1 = new ZonedTime(11, 50, 5, new TimeZone(1));
+        ZonedTime zt2 = new ZonedTime(4, 12, 40, new TimeZone(-3));
 
+        System.out.println(zt1.secondsBetween(zt2));
+//34645
+
+//71892
     }
 }
