9c34aee19e8294c1b4213c2daf667e13046a22ec
brightside22 <brightside227@gmail.com>
2023-11-13 20:15:11 +0300

    Homework H17
    Signed-off-by: brightside22 <brightside227@gmail.com>

--- src/ru/progwards/java1/lessons/inheritance/ZonedTime.java
+++ src/ru/progwards/java1/lessons/inheritance/ZonedTime.java
@@ -21,12 +21,15 @@ public class ZonedTime extends Time {
     @Override
     public int secondsBetween(Time time) {
         int tmz = 0;
+        int tmz1 = 0;
+
 
         if (this.zone != null) {
             tmz = zone.getHours()  *  3600 + zone.getMinutes()  *  60;
+            tmz1 = TimeZone.hours *  3600 + TimeZone.minutes  *  60;
         }
 
-        int sec= this.hours  *  3600 + this.minutes  *  60 + this.seconds - tmz;
+        int sec= this.hours  *  3600 + this.minutes  *  60 + this.seconds- tmz1;
         int sec1 = super.hours  *  3600 + super.minutes  *  60 + super.seconds;
 
         if (sec >= sec1) {
@@ -47,10 +50,17 @@ public class ZonedTime extends Time {
         System.out.println(ztt1.secondsBetween(ztt2));
         // 31720
 
+        ZonedTime zttt1 = new ZonedTime(7, 2, 41, new TimeZone(-2));
+        ZonedTime zttt2 = new ZonedTime(21, 1, 43, new TimeZone(-2));
+        System.out.println(zttt1.secondsBetween(zttt2));
+        //35942
+
         ZonedTime z1 = new ZonedTime(17, 34, 55, new TimeZone(0, 9));
         Time t1 = new Time(7, 39, 29);
         System.out.println(z1.secondsBetween(t1));
         //35186
+
+
     }
 }
 
