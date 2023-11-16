fa86ef866187fbb75986f0387e320a50e5c0e68f
brightside22 <brightside227@gmail.com>
2023-11-17 00:04:20 +0300

    Homework H17
    Signed-off-by: brightside22 <brightside227@gmail.com>

--- src/ru/progwards/java1/lessons/arrays1/Eratosthenes.java
+++ src/ru/progwards/java1/lessons/arrays1/Eratosthenes.java
@@ -4,34 +4,20 @@ public class Eratosthenes {
     private boolean[] sieve;
 
     public Eratosthenes(int N) {
-        sieve = new boolean[N + 1];
-
-
+        sieve = new boolean[N];
         for (int i = 2; i <= N; i++) {
             sieve[i] = true;
         }
-
-
         sift();
     }
 
     private void sift() {
-        for (int i = 2; i * i <= sieve.length; i++) {
-
+        for (int i = 2; i * i < sieve.length-1; i++) {
             if (sieve[i]) {
-
-
-                for (int j = i * i; j <= sieve.length; j += i) {
+                for (int j = i * i; j < sieve.length-1; j += i) {
                     sieve[j] = false;
                 }
-
-
             }
         }
     }
-
-    public boolean isSimple(int n) {
-        return sieve[n];
-    }
 }
-
