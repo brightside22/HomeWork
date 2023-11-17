cf1f54979362308c30e45a93b60e12b3e7659a69
brightside22 <brightside227@gmail.com>
2023-11-17 23:05:05 +0300

    Homework H17
    Signed-off-by: brightside22 <brightside227@gmail.com>

--- src/ru/progwards/java1/lessons/arrays1/Matrix.java
+++ src/ru/progwards/java1/lessons/arrays1/Matrix.java
@@ -8,83 +8,68 @@ public class Matrix {
     }
 
     public int maxInRow(int num) {
-        int min = 0;
-        for (int i = 0; i < matrix[num].length; i++) {
-            if (min < matrix[num][i])
-                min = matrix[num][i];
+        int max = matrix[num][0];
+        for (int i = 1; i < matrix[num].length; i++) {
+            if (max < matrix[num][i])
+                max = matrix[num][i];
         }
-        return min;
+        return max;
     }
+
+
+   /* -9   7  -7
+      -4  -2  -2
+       3   3   7
+      -8   1  -1
+    Возвращенo: 0. Ожидалось: -2. */
 
     public int maxInCol(int num) {
-        int n = 0;
-
-        for (int i = 0; i < matrix.length; i++) {
-            if (matrix!= null) {
-                if (matrix[i][num] > matrix[i + 1][num]) {
-                    n = matrix[i][num];
-                } else if (matrix[i][num] < matrix[i + 1][num]) {
-                    n = matrix[i][num];
-                }
+        int max = matrix[0][num];
+        for (int i = 1; i < matrix.length; i++) {
+            if (matrix[i][num] > max) {
+                max = matrix[i][num];
             }
         }
-        return n;
+        return max;
     }
 
+
     public int max() {
-        int min = 0;
+        int max = 0;
 
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[i].length; j++) {
                 {
-                    if (min < matrix[i][j])
-                        min = matrix[i][j];
+                    if (max < matrix[i][j])
+                        max = matrix[i][j];
                 }
             }
         }
-        return min;
+        return max;
     }
-/*      -4  -8   5
-        -4  -4  -8
-         7  -4   0
-         6   3   8
-    Возвращенo: 12. Ожидалось: 8. */
+
 
     public boolean isMatrix() {
-        int counter = 0;
-        int counter1 = 0;
-        boolean t = false;
         for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[i].length; j++) {
-                counter += 1;
+            if (matrix.length != matrix[i].length) {
+                return false;
             }
-        for (int j = 1; j < matrix[i].length; i++) {
-                counter += 1;
-            }
-        if (counter1 == counter){
-            t = true;
         }
-        else
-            t = false;
-        }
-        return t;
+        return true;
     }
+
+
     public int[][] transposition() {
-        if (isMatrix() != true)
+        if (!isMatrix()) {
             return null;
+        }
 
-        int a = 0;
-        int b = 0;
         for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[i].length; j++) {
-                a = matrix[i][j];
-                b = matrix[i+1][j];
-                matrix[i+1][j] = a;
-                matrix[i][j]=b;
-
+            for (int j = i + 1; j < matrix[i].length; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
             }
         }
         return matrix;
     }
-
- }
