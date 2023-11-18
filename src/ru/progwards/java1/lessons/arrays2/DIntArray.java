18e30007a920dbd9d2f4c44971a48c4b29deabf8
brightside22 <brightside227@gmail.com>
2023-11-18 21:03:08 +0300

    Homework H20
    Signed-off-by: brightside22 <brightside227@gmail.com>

--- src/ru/progwards/java1/lessons/arrays2/DIntArray.java
+++ src/ru/progwards/java1/lessons/arrays2/DIntArray.java
@@ -4,43 +4,45 @@ import java.util.Arrays;
 
 public class DIntArray {
 
-    private int[] a = new int[0];
+    private int[] a = new int[10];
 
     public DIntArray() {
     }
+
     public void add(int num) {
         int[] ad = Arrays.copyOf(a, a.length + 1);
-        ad[ad.length - 1] = num;
-        a = ad;
+        for (int i = 0; i < a.length; i++) {
+            ad[i] = a[i];
+            ad[i + 1] = num;
+        }
     }
 
 
     public void atInsert(int pos, int num) {
-        Arrays.copyOf(a, a.length+1);
+        Arrays.copyOf(a, a.length + 1);
         for (int i = pos; i <= a.length; i++) {
-            a[i] = a[i+1];
+            a[i] = a[i + 1];
             a[pos] = num;
         }
     }
 
 
     public void atDelete(int pos) {
-        Arrays.copyOf(a, a.length-1);
+        Arrays.copyOf(a, a.length - 1);
         for (int i = pos; i < a.length; i++) {
-            a[i] =  a[i-1];
+            a[i] = a[i - 1];
         }
     }
 
 
-    public  int at(int pos)  {                 //- возвращает элемент по индексу pos
+    public int at(int pos) {                 //- возвращает элемент по индексу pos
         Arrays.sort(a);
         return Arrays.binarySearch(a, pos);
     }
 
 
-    public static void main(String[] args) {
+    public void main(String[] args) {
         int[] a = {1, 2, 3, 4, 5, 6};
-
     }
-
 }
+
