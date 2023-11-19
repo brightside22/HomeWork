60c47c1d3968aeab2e0271eeec860cefca838f56
brightside22 <brightside227@gmail.com>
2023-11-19 12:54:18 +0300

    Homework H20.4
    Signed-off-by: brightside22 <brightside227@gmail.com>

--- src/ru/progwards/java1/lessons/arrays2/DIntArray.java
+++ src/ru/progwards/java1/lessons/arrays2/DIntArray.java
@@ -7,25 +7,24 @@ public class DIntArray {
     private int[] a;
 
     public DIntArray() {
-     a = new int [10];
+        a = new int[10];
     }
 
-    public void add(int num)  {                          //добавляет элемент num в конец массива
+    public void add(int num) {                          //добавляет элемент num в конец массива
         int[] ad = Arrays.copyOf(a, a.length + 1);
         for (int i = 0; i < a.length; i++) {
             ad[i] = a[i];
         }
-        ad[ad.length-1] = num;
+        ad[ad.length - 1] = num;
         a = ad;
     }
-
 
 
     public void atInsert(int pos, int num) {            //добавляет элемент num в позицию pos
 
         int[] aa = Arrays.copyOf(a, a.length + 1);
-        for (int i = pos; i < aa.length; i++ ) {
-            aa[i] = a[i+1];
+        for (int i = pos; i < aa.length; i++) {
+            aa[i] = a[i + 1];
         }
         aa[pos] = num;
         a = aa;
@@ -36,17 +35,14 @@ public class DIntArray {
 
         int[] aa = new int[a.length - 1];
         for (int i = pos; i < a.length; i++) {
-            aa[i] = a[i-1];
+            aa[i] = a[i - 1];
         }
         a = aa;
     }
 
 
-    public int at(int pos)  {                 //- возвращает элемент по индексу pos
-        Arrays.sort(a);
-      int aaa = Arrays.binarySearch(a, pos);
-
-      return a[aaa];
+    public int at(int pos) {
+        return a[pos];
     }
 }
 
