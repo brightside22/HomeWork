19a0d549e9c9de6880bffca4eeef264f0143e6c7
brightside22 <brightside227@gmail.com>
2023-11-19 13:01:04 +0300

    Homework H20.4
    Signed-off-by: brightside22 <brightside227@gmail.com>

--- src/ru/progwards/java1/lessons/arrays2/DIntArray.java
+++ src/ru/progwards/java1/lessons/arrays2/DIntArray.java
@@ -10,12 +10,9 @@ public class DIntArray {
         a = new int[10];
     }
 
-    public void add(int num) {                          //добавляет элемент num в конец массива
+    public void add(int num) {               //добавляет элемент num в конец массива
         int[] ad = Arrays.copyOf(a, a.length + 1);
-        for (int i = 0; i < a.length; i++) {
-            ad[i] = a[i];
-        }
-        ad[ad.length - 1] = num;
+        ad[a.length] = num;
         a = ad;
     }
 
@@ -23,8 +20,8 @@ public class DIntArray {
     public void atInsert(int pos, int num) {            //добавляет элемент num в позицию pos
 
         int[] aa = Arrays.copyOf(a, a.length + 1);
-        for (int i = pos; i < aa.length; i++) {
-            aa[i] = a[i + 1];
+        for (int i = pos + 2; i < aa.length; i++) {
+            aa[i - 1] = a[i];
         }
         aa[pos] = num;
         a = aa;
@@ -32,9 +29,8 @@ public class DIntArray {
 
 
     public void atDelete(int pos) {              //удаляет элемент в позиции pos массива
-
         int[] aa = new int[a.length - 1];
-        for (int i = pos; i < a.length; i++) {
+        for (int i = pos; i < aa.length; i++) {
             aa[i] = a[i - 1];
         }
         a = aa;
