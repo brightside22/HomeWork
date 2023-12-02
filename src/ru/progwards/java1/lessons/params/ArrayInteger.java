e21b10dab2ef0d5382bf43fa6e393872025c3c78
brightside22 <brightside227@gmail.com>
2023-12-02 18:16:10 +0300

    HomeWork new 23
    Signed-off-by: brightside22 <brightside227@gmail.com>

--- src/ru/progwards/java1/lessons/params/ArrayInteger.java
+++ src/ru/progwards/java1/lessons/params/ArrayInteger.java
@@ -1,11 +1,13 @@
 package ru.progwards.java1.lessons.params;
 import java.util.Arrays;
+
 public class ArrayInteger {
     private byte[] digits;
 
 
     ArrayInteger(int n) {
-        digits = new byte[n];
+     String n1 = String.valueOf(n);
+     char [] nChar = n1.toCharArray();
     }
 
     void fromString(String value) {
@@ -24,35 +26,47 @@ public class ArrayInteger {
        byte[] num1 = this.digits;
        byte[] num2 = num.digits;
        byte[] num3 = {};
-       int count = 0;
-       int count1 = 0;
+       byte sum = 0;
+       byte perepol = 0;
 
-       for(int i = 0; i<=num1.length; i++) {
-           count++;
-       }
+        int len1 = num1.length;
+        int len2 = num2.length;
+        int count = Math.max(len1, len2);
 
-       for(int i = 0; i<=num2.length; i++) {
-           count1++;
-       }
-
-       if (count1 >= count){
+       if (count >= count){
             for (int i = count; i<count; i--){
-
+                sum = (byte) (num1[i] + num2[i]);
+                    num3[i-1] += sum %10;
+                    perepol = (byte) (sum/10);
             }
         }
 
-       else {
-           for (int i = count1; i>=count1; i--) {
-
-           }
-       }
-
-        if () {
-
+        if (perepol != 0) {
             return false;
         }
+
+        this.digits = Arrays.copyOf(num3, count);
+        return true;
     }
 
+
+    public static void main(String[] args) {
+        ArrayInteger num1 = new ArrayInteger(5);
+        num1.fromString("12345");
+        System.out.println("num1: " + num1.toString());
+
+        ArrayInteger num2 = new ArrayInteger(5);
+        num2.fromString("98765");
+        System.out.println("num2: " + num2.toString());
+
+        boolean added = num1.add(num2);
+        if (added) {
+            System.out.println("Sum: " + num1.toString());
+        } else {
+            System.out.println("Overflow! Result reset to 0.");
+            System.out.println("Sum: " + num1.toString());
+        }
+    }
 
 
 }
