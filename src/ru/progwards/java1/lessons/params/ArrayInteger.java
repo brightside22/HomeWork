1f7c6bc400f07ae72e2db7acdd42ff8667236595
brightside22 <brightside227@gmail.com>
2023-12-03 11:12:59 +0300

    HomeWork new 23
    Signed-off-by: brightside22 <brightside227@gmail.com>

--- src/ru/progwards/java1/lessons/params/ArrayInteger.java
+++ src/ru/progwards/java1/lessons/params/ArrayInteger.java
@@ -18,7 +18,7 @@ public class ArrayInteger {
 
     public String toString() {
         String numStr = "";
-        for(int i =0; i<digits.length;i++) {
+        for(int i =0; i<digits.length; i++) {
             numStr += String.valueOf(digits[i]);
         }
         return numStr;
@@ -27,22 +27,32 @@ public class ArrayInteger {
     public boolean add(ArrayInteger num) {
        byte[] num1 = this.digits;
        byte[] num2 = num.digits;
-       byte sum = 0;
+       int sum = 0;
        byte perepol = 0;
 
-        int len1 = num1.length;
-        int len2 = num2.length;
-        int count = Math.max(len1, len2);
+        int leng1 = num1.length;
+        int leng2 = num2.length;
+        int count = Math.max(leng1, leng2);
 
         byte[] num3 = new byte[count + 1];
 
 
-       if (count >= count){
-            for (int i = count; i<count & i >=0; i--){
-                sum = (byte) (num1[i] + num2[i]);
-                    num3[i-1] += sum %10;
-                    perepol = (byte) (sum/10);
+        for (int i = count - 1; i >= 0; i--) {
+
+            if (i < leng1 && i < leng2) {
+                sum = num1[i] + num2[i] + perepol;
             }
+
+            else if (i < leng1) {
+                sum = num1[i] + perepol;
+            }
+
+            else {
+                sum = num2[i] + perepol;
+            }
+
+            num3[i] = (byte) (sum % 10);
+            perepol = (byte) (sum / 10);
         }
 
         if (perepol != 0) {
@@ -50,23 +60,28 @@ public class ArrayInteger {
         }
 
         else {
-            this.digits = Arrays.copyOf(num3, count);
+            num1 = Arrays.copyOf(num3, count);
             return true;
         }
     }
 
 
     public static void main(String[] args) {
-        ArrayInteger num1 = new ArrayInteger(5);
-        num1.fromString("12345");
+        ArrayInteger num1 = new ArrayInteger(7);
+        num1.fromString("1140955");
         System.out.println("num1: " + num1.toString());
 
-        ArrayInteger num2 = new ArrayInteger(5);
-        num2.fromString("99999");
+        ArrayInteger num2 = new ArrayInteger(6);
+        num2.fromString("100792");
         System.out.println("num2: " + num2.toString());
 
         boolean added = num1.add(num2);
-            System.out.println("Sum: " + num1.add(num2));
+        System.out.println("Sum: " + num1.add(num2));
+
+        System.out.println("num1: " + num1.toString());
+
+
+
 
     }
 
