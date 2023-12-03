1730e30705c32bd98bc94b5c4a2e51143f08f60d
brightside22 <brightside227@gmail.com>
2023-12-03 12:08:05 +0300

    HomeWork new 23
    Signed-off-by: brightside22 <brightside227@gmail.com>

--- src/ru/progwards/java1/lessons/params/ArrayInteger.java
+++ src/ru/progwards/java1/lessons/params/ArrayInteger.java
@@ -6,7 +6,7 @@ public class ArrayInteger {
 
 
     ArrayInteger(int n) {
-        byte[] digits = new byte [n];
+        digits = new byte[n];
     }
 
     void fromString(String value) {
@@ -25,14 +25,15 @@ public class ArrayInteger {
     }
 
     public boolean add(ArrayInteger num) {
-       byte[] num1 = this.digits;
-       byte[] num2 = num.digits;
-       int sum = 0;
-       byte perepol = 0;
+        byte[] num1 = this.digits;
+        byte[] num2 = num.digits;
+        int sum = 0;
+        byte perepol = 0;
 
         int leng1 = num1.length;
         int leng2 = num2.length;
         int count = Math.max(leng1, leng2);
+
 
         if (count == leng1) {
             byte[] num4 = new byte [num1.length];
@@ -57,12 +58,23 @@ public class ArrayInteger {
 
 
 
-        byte[] num3 = new byte[count];
+
+        byte[] num3 = new byte[count+1];
 
 
         for (int i = count - 1; i >= 0; i--) {
+            if (i < leng1 && i < leng2) {
                 sum = num1[i] + num2[i] + perepol;
+            }
 
+            else if (i < leng1) {
+                sum = num1[i] + perepol;
+            }
+
+
+            else {
+                sum = num2[i] + perepol;
+            }
 
             num3[i] = (byte) (sum % 10);
             perepol = (byte) (sum / 10);
@@ -85,11 +97,11 @@ public class ArrayInteger {
 
 
     public static void main(String[] args) {
-        ArrayInteger num1 = new ArrayInteger(7);
-        num1.fromString("15711584");
+        ArrayInteger num1 = new ArrayInteger(8);
+        num1.fromString("1571154");
         System.out.println("num1: " + num1.toString());
 
-        ArrayInteger num2 = new ArrayInteger(6);
+        ArrayInteger num2 = new ArrayInteger(5);
         num2.fromString("31578");
         System.out.println("num2: " + num2.toString());
 
