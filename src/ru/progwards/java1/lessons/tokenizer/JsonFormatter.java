d3c517cded7eea174c2feba0602bb9716b9ad08c
brightside22 <brightside227@gmail.com>
2024-01-23 13:30:35 +0300

    HomeWork new 25
    Signed-off-by: brightside22 <brightside227@gmail.com>

--- src/ru/progwards/java1/lessons/tokenizer/JsonFormatter.java
+++ src/ru/progwards/java1/lessons/tokenizer/JsonFormatter.java
@@ -3,26 +3,26 @@ package ru.progwards.java1.lessons.tokenizer;
 public class JsonFormatter {
     public static String format(String json) {
         int indentLevel = 0;
-        String formattedJson = "";
+        StringBuilder formattedJson = new StringBuilder();
 
         for (char ch : json.toCharArray()) {
             switch (ch) {
                 case '{':
                     indentLevel++;
-                    formattedJson += "{\n" + format(String.valueOf(indentLevel * 2));
+                    formattedJson.append("{\n").append(format(String.valueOf(indentLevel * 2)));
                     break;
                 case '}':
                     indentLevel--;
-                    formattedJson += "\n" + format(String.valueOf(indentLevel * 2)) + "}";
+                    formattedJson.append("\n").append(format(String.valueOf(indentLevel * 2))).append("}");
                     break;
                 case ',':
-                    formattedJson += ",\n" + format(String.valueOf(indentLevel * 2));
+                    formattedJson.append(",\n").append(format(String.valueOf(indentLevel * 2)));
                     break;
                 default:
-                    formattedJson += ch;
+                    formattedJson.append(ch);
             }
         }
 
-        return formattedJson;
+        return formattedJson.toString();
     }
 }
